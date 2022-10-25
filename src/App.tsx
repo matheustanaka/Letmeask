a905bbfa4f5ff7c914e2ed98e595238102714db2
Matheus Tanaka <matheustanakanog@gmail.com>
2022-10-25 14:51:04 -0300

    refactor: remove context and auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { createContext, useState, useEffect } from "react";
-
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { auth, firebase } from "./services/firebase";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
 import { Room } from "./pages/Room";
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -8,7 +8,6 @@ import answerImg from "../assets/images/answer.svg";
 import { Button } from "../components/Button";
 import { Question } from "../components/Question";
 import { RoomCode } from "../components/RoomCode";
-// import { useAuth } from '../hooks/useAuth';
 import { useRoom } from "../hooks/useRoom";
 import { database } from "../services/firebase";
 
